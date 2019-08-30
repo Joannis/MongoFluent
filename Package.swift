f6da0a21ecb78e987a4bc3cf85ccb2734c1a3a33
Reda Lemeden <git@redalemeden.com>
2019-08-30 17:13:06 +0100

    Use remote dependencies

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(path: "/Users/joannisorlandos/Projects/OpenKitten/MongoKitten"),
-        .package(url: "https://github.com/vapor/fluent-kit.git", .revision("master")),
+        .package(url: "https://github.com/OpenKitten/MongoKitten.git", .branch("master/6.0")),
+        .package(url: "https://github.com/vapor/fluent-kit.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
